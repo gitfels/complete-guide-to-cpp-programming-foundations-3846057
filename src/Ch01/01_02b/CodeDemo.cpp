91af7efc7ff391f76d39da426c91ba30eac02e4e
gitfels <garrett.sidhom0@gmail.com>
2026-09-11 15:59:21 +0000

    lesson 01_02

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,21 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int add(int num1, int num2){
+    return num1+num2;
+}
+int main(){
+    int x,y;
+    std::cout << "First num?" << std::endl;
+    std::cin >> x;
+
+    std::cout << "Second num?" << std::endl;
+    std::cin >> y;
+
+    int result = add(x, y);
+    std::cout << "The result of adding " << x << " to " << y << " is " << result << std::endl;
+    return 0;
+}
